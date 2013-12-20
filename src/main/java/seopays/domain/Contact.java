838c15ff7b3cb6769df9179c00e582cf6e794b85
Vladimir Sidorenko <vladimir.sidorenko@dune-hd.com>
2013-12-20 12:12:40 +0200

    Fix for connection to db.

--- src/main/java/seopays/domain/Contact.java
+++ src/main/java/seopays/domain/Contact.java
@@ -27,5 +27,43 @@ public class Contact {
     @Column(name = "TELEPHONE")
     private String telephone;
 
-    // Getters and setters
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 }
