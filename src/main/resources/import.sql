858a9a08356fe602a90c9539c6bd66d2136b60f1
Vladimir Sidorenko <vladimir.sidorenko@dune-hd.com>
2013-12-24 16:47:47 +0200

    some TODO-s.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+
+insert into users values ('admin', 'admin', true);
+
+insert into authorities (username, authority) values ('admin', 'ROLE_ADMIN');
+insert into authorities (username, authority) values ('admin', 'ROLE_USER');
