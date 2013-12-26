47f5d538b92187839f3fd31fe845d383cc8196ef
Vladimir Sidorenko <vladimir.sidorenko@dune-hd.com>
2013-12-26 17:08:08 +0200

    SimpleGrantedAuthority

--- src/main/java/seopays/security/CrowdAuthenticationProvider.java
+++ src/main/java/seopays/security/CrowdAuthenticationProvider.java
@@ -5,6 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CrowdAuthenticationProvider implements AuthenticationProvider {
 
@@ -18,7 +23,16 @@ public class CrowdAuthenticationProvider implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) {
 
+        String name = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        if (name.equals("admin") && password.equals("system")) {
+            List<GrantedAuthority> grantedAuths = new ArrayList<GrantedAuthority>();
+            grantedAuths.add(new SimpleGrantedAuthority("ROLE_USER"));
+            Authentication auth = new UsernamePasswordAuthenticationToken(name, password, grantedAuths);
 
-        return null;
+            return auth;
+        } else {
+            return null;
+        }
     }
 }
